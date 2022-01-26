96f998977e679bdcb57ed506f37a483d8fce501f
Nishant Gerald <nishant.gerald@gmail.com>
2022-01-26 16:15:42 -0500

    removed everything

--- routes/proxy.js
+++
@@ -1,9 +0,0 @@
-var httpProxy = require('http-proxy');
-var proxy = httpProxy.createProxyServer({
-  target: 'http://www.j-archive.com/'
-});
-
-module.exports = function (req, res) {
-  delete req.headers.referer;
-  proxy.web(req, res);
-}
